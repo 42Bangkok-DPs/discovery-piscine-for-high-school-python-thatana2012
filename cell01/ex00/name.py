fe0bb569839faf21943c7ce0d4f1db76bcaa8bbf
Thatana Jerdnapapun <tjerdnap@z2t8c4.42bangkok.com>
2024-10-09 12:25:54 +0700

    add cell01: ex00, ex01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Thatana"
+last_name = "Jerdnapapun"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Thatana"
+last_name = "Jerdnapapun"
+whole_name = first_name + " " + last_name
+print(whole_name)
