dc8d2a1e682f96299dbc6519f4410e351f152909
Thatana Jerdnapapun <tjerdnap@z2t8c3.42bangkok.com>
2024-10-10 15:57:30 +0700

    add cell3.1

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
-number = int(input("Enter a number\n"))
-start = int(0)
-while (start <= 9):
-    result = start * number
-    print(f"{start} x {number} = {result}")
-    start += 1
+#!/usr/bin/python3
+number = input("Enter a number\n")
+try:
+    val = int(number)
+except ValueError:
+    print("That's not an int!")
+else:
+    start = 0
+    while (start <= 9):
+        result = start * int(number)
+        print(f"{start} x {number} = {result}")
+        start += 1
