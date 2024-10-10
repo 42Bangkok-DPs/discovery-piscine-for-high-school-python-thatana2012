cb8ca25a99d940bef7a2d7ba10f20cbe7c6b7612
Thatana Jerdnapapun <tjerdnap@z2t8c3.42bangkok.com>
2024-10-10 16:03:01 +0700

    add cell2.3

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-first_num = int(input("Enter the first number:\n"))
-second_num = int(input("Enter the second number:\n"))
-result = first_num * second_num
-print(f"{first_num} x {second_num} = {result}")
-if result < 0:
-    print("The result is negative.")
-elif result > 0:
-    print("The result is positive.")
+first_num = input("Enter the first number:\n")
+second_num = input("Enter the second number:\n")
+try:
+    val1 = int(first_num)
+    val2 = int(second_num)
+except ValueError:
+    print("Error")
 else:
-    print("The result is positive and negative.")
+    result = int(first_num) * int(second_num)
+    print(f"{first_num} x {second_num} = {result}")
+    if result < 0:
+        print("The result is negative.")
+    elif result > 0:
+        print("The result is positive.")
+    else:
+        print("The result is positive and negative.")
